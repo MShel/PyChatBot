34496aca42d891d02449d723ba4accdb7aadafcf
Ubuntu <ubuntu@ip-172-31-62-247.ec2.internal>
2017-02-16 18:12:43 +0000

    just a fix for recipe plugin in case if nothing found

--- bot_plugins/Recipy.py
+++ bot_plugins/Recipy.py
@@ -1,9 +1,8 @@
 import requests
-import json
 import urllib
 
-class Recipy:
 
+class Recipy:
     welcome_message = 'Type where and when now \n'
 
     storage = None
@@ -15,7 +14,8 @@ class Recipy:
         self.sender_id = sender_id
 
     def get_help_message(self):
-        return 'Type Ex. Ingredients:meat,egg,potatos and get list of links in response'
+        return 'Type comma separated ingredients(meat,egg,potatos)\n' \
+               'and get list of links in response'
 
     def get_response(self, message):
         url = 'http://www.recipepuppy.com/api/?i=' + urllib.quote(message) + '&p=1'
@@ -23,13 +23,16 @@ class Recipy:
         request_recipe = requests.get('http://www.recipepuppy.com/api/?i=' + urllib.quote(message) + '&p=1')
         print(request_recipe)
         result = ''
-        print(request_recipe.json())
-        i = 0
-        for result_recipe in request_recipe.json()['results']:
-            result += '\n'
-            result += result_recipe['title'].encode('utf-8') + ' - '
-            result += result_recipe['href']
-            i+=1
-            if i>3:
-                break
+        try:
+       	    print(request_recipe.json())
+            i = 0
+            for result_recipe in request_recipe.json()['results']:
+                result += '\n'
+                result += result_recipe['title'].encode('utf-8') + ' - '
+                result += result_recipe['href']
+                i += 1
+                if i > 3:
+                    break
+        except Exception:
+	    result += 'Nothing found your ingredients :( Please try something else' 
         return result
