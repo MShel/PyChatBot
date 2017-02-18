9040878f12c3f886b79b4919fe7e1e33325badd0
Mishk0 <mshelemetev@gmail.com>
2017-02-18 13:50:18 -0500

    cleanup of smelly code, still smell but less

---
+++ bot_plugins/AbstractPlugin.py
@@ -0,0 +1,25 @@
+from abc import ABCMeta, abstractmethod
+
+'''
+all plugins should extend this class
+and implement missing methods
+'''
+class AbstractPlugin:
+
+    __metaclass__ = ABCMeta
+
+    MESSAGE_MAX_TEXT_LEN = 640
+
+    @abstractmethod
+    def get_help_message(self): pass
+
+
+    @abstractmethod
+    def get_response(self): pass
+
+
+    @abstractmethod
+    def format_output(self): pass
+
+    @abstractmethod
+    def validate_message(self):pass
