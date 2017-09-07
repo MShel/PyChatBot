fc7749307a54314dc7fc5cc916e6ca8899b286a8
misha shelemetyev <misha.shelemetyev@onforce.com>
2017-09-07 17:19:46 -0400

    router is now fully covered start thinking about transports and configs

--- tests/RouterTest.py
+++ tests/RouterTest.py
@@ -1,20 +1,19 @@
 import unittest
 import Router
 from bot_plugins import Weather
-
+from mock import Mock
 
 class DummyStorage:
-    def get(self):
+    def get(self, *args):
         pass
-
-    def set(self):
+    def set(self, *args):
         pass
-
-    def setex(self,*args):
+    def setex(self, *args):
         pass
 
 
 class RouterTest(unittest.TestCase):
+
     def test_success(self):
         storage = DummyStorage()
         testRouter = Router.Router(storage)
@@ -23,6 +22,20 @@ class RouterTest(unittest.TestCase):
         weatherPlug.sender_id = 1
         self.assertEqual(testRouter.get_plugin('weather', 1), (weatherPlug,False))
 
+    def test_success_initialised_plugin(self):
+        storage = DummyStorage()
+        storage.get = Mock()
+        storage.get.return_value = 'weather'
+        testRouter = Router.Router(storage)
+        weatherPlug = Weather.Weather()
+        weatherPlug.storage = storage
+        weatherPlug.sender_id = 1
+        self.assertEqual(testRouter.get_plugin('trash', 1), (weatherPlug, True))
+
+    def test_invalid_plugin(self):
+        storage = DummyStorage()
+        testRouter = Router.Router(storage)
+        self.assertEqual(testRouter.get_plugin('InvalidPlugin', 1), (None,False))
 
 if __name__ == '__main__':
     unittest.main()
