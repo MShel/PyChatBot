1984d6985e2386b848d60b63b7ac1737bb86763f
misha shelemetyev <misha.shelemetyev@onforce.com>
2017-10-11 10:40:32 -0400

    Dog Plugin up and running

---
+++ tests/plugins/DogPluginTest.py
@@ -0,0 +1,18 @@
+from bot_plugins.DogPlugin import DogPlugin
+import unittest
+
+class DogPluginTest(unittest.TestCase):
+
+    def test_success(self):
+        dog_plugin = DogPlugin()
+        response = dog_plugin.get_response('beagle')
+        self.assertEqual(True, 'api/img' in response)
+
+    def test_failure(self):
+        dog_plugin = DogPlugin()
+        response = dog_plugin.get_response('1beagle')
+        self.assertEqual("Nothing found for that dog breed :( Please try something else",response)
+
+
+if __name__ == '__main__':
+    unittest.main()
